805f3d641e26824e209689d0af1408783ec8ccbc
Kevin Patel <kevinpatel907.kp@gmail.com>
2025-08-23 19:44:31 +0530

    Finished http layer.

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,34 +2,104 @@ import express from "express"
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config"; //! instead of config, use env
 import { middleware } from "./middleware";
+import bcrypt from "bcrypt"
+import {prismaClient} from "@repo/db/client"
 import {CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types"
 const app = express()
+app.use(express.json());
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
     //! Add zod validations here
-    const data = CreateUserSchema.safeParse(req.body);
-    if (!data.success) {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
         return res.status(400).json({ error: "Invalid data" });
     }
-    res.json({ message: "Signup successful" });
+    const { username, password, name } = parsedData.data;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    try {
+        let user_with_email = await prismaClient.user.findUnique({
+            where: {
+                email: username
+            }
+        }) 
+
+        if (user_with_email) {
+            return res.status(400).json({ error: "Email already in use" });
+        }
+
+        const user = await prismaClient.user.create({
+            data: {
+                email: username,
+                password: hashedPassword,
+                name: name,
+            }
+        })
+        res.json({ message: "Signup successful", userId: user.id });
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
-app.post("/signin", (req ,res) => {
-    const data = SigninSchema.safeParse(req.body);
-    if (!data.success) {
+app.post("/signin", async (req ,res) => {
+    const parsedData = SigninSchema.safeParse(req.body);
+    if (!parsedData.success) {
         return res.status(400).json({ error: "Invalid data" });
     }
-    const userId = 1;
-    const token = jwt.sign({ userId }, JWT_SECRET);
-    res.json({ token });
+    const {username, password} = parsedData.data;
+    try {
+        const user = await prismaClient.user.findUnique({
+            where: {
+                email: username,
+            }
+        });
+    
+        if (!user) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+
+        const match_password = await bcrypt.compare(password, user.password);
+        if (!match_password) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET);
+        res.json({ token });
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error" });
+    }
 })
 
-app.post("/room", middleware, (req, res) => {
-    const data = CreateRoomSchema.safeParse(req.body);
-    if (!data.success) {
+app.post("/room", middleware, async (req, res) => {
+    const parsedData = CreateRoomSchema.safeParse(req.body);
+    if (!parsedData.success) {
         return res.status(400).json({ error: "Invalid data" });
     }
-    res.json({ message: "Room created successfully" });
+
+    // @ts-ignore //! Todo fix this
+    const userId = req.userId;
+    const {name} = parsedData.data;
+    try{
+        const room_exist = await prismaClient.room.findUnique({
+            where: {
+                slug: name
+            }
+        })
+
+        if (room_exist) {
+            return res.status(400).json({ error: "Room name already in use" });
+        }
+    
+        const room = await prismaClient.room.create({
+            data: {
+                slug: name,
+                adminId: userId
+            }
+        });
+        res.json({ message: "Room created successfully", roomId: room.id });
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
 })
 
 app.listen(3001)
